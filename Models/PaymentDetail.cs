a10a2c803879a115a8079e6dc3034de37cd8d975
Muhammad Mahtab Ahmed <38474783+Muhammadmahtab@users.noreply.github.com>
2024-06-06 16:47:12 +0500

    Add files via upload

---
+++ Models/PaymentDetail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace paymentApi.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailsId { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; } = "";
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; } = "";
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; } = "";
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; } = "";
+    }
+}
---
+++ Models/PaymentDetailContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace paymentApi.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
